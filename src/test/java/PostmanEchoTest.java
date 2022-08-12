6523d852833a2264ad99f233c0fc34b040b41b59
Vadim Piskarev <woxadestroyer@gmail.com>
2022-08-13 00:34:10 +0300

    initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+    @Test
+    void shouldReturnDemoBody() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello world!")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello world!"))
+        ;
+    }
+}
